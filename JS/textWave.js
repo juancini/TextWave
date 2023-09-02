e79506a0617b59cdecbb0f9a63559ab8d6d8080c
juani mancini <juani.mancini98.com>
2023-09-02 18:51:58 -0300

    added string changing when hovered

--- JS/textWave.js
+++ JS/textWave.js
@@ -1,4 +1,21 @@
-function generateWave() {
-    document.getElementById("targetParagraph").innerText =
-    "Another text to change";
+function generateWave(len) {
+    // Returns a random string
+    // Inputs:
+    //      len :: int
+    //      length of strings
+    
+    let randomString = '';
+    let chars = 'abcdefghijklmnoprsuvwyzABCDEFGHIJKLMNOPQRSTUVW1234567890!"#$%&()=-_!';
+    for (let stringPlace = 0; stringPlace < len; stringPlace++) {
+        randomString += chars.charAt(Math.floor(Math.random() * 69))
+    }
+    return randomString;
 }
+
+function updateText() {
+    // Updates text
+
+    document.getElementById("targetText").innerHTML = generateWave(2000);
+}
+
+console.log("Import OK.")
